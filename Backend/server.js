1bea5edabf3b5e9e8caa8fa567a2188cd91a692e
sejalkutriyar <seju020322@gmail.com>
2025-05-05 13:58:35 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,28 @@
 require('./db.js');
 
-mongo url: 'mongodb+srv://seju020322:iB#fMpmLY_y4vNe@cluster0.bnihqqy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db.js');
+const recipeRoutes = require('./Routes/recipe.js');
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/recipes', recipeRoutes);
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+connectDB();
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
